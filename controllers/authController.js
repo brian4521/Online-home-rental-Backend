b9d8971a9c6bb002cb8e4ce29d1dec39f6447121
donald <warnerroby6@gmail.com>
2026-04-20 21:07:00 +0545

    signup validation done in server side

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const { check, validationResult } = require("express-validator");
 
 
 exports.getlogin = (req, res, next) => {
@@ -5,6 +6,78 @@ exports.getlogin = (req, res, next) => {
 
 
 };
+
+
+exports.postsignup = [
+
+
+   check('firstName')
+   .trim()
+   .isLength({min:2})
+   .withMessage("first name should be at least of 2 character")
+   .matches(/^[A-Za-z\s]+$/)
+   .withMessage("first name should contain only alphabets"),
+
+   check('lastName')
+   .matches(/^[A-Za-z\s]*$/)
+   .withMessage("first name should contain only alphabets"),
+
+   check('email')
+   .isEmail()
+   .withMessage("Please enter a valid email")
+   .normalizeEmail(),
+
+   check('password')
+   .isLength({min:8})
+   .withMessage("Password should be at least 8 character long")
+   .matches(/[A-Z]/)
+   .withMessage("password should contain at least one uppercase")
+   .matches(/[a-z]/)
+   .withMessage("password should contain at least one lowercase")
+   .matches(/[0-9]/)
+   .withMessage("password should contain at least one number")
+   .matches(/[!@&]/)
+   .withMessage("password should contain at least one special character")
+   .trim(),
+
+   check('confirmPassword')
+   .custom((value,{req})=>{
+      if(value !== req.body.password){
+         throw new Error("Passwords do not match");
+      }
+      return true; 
+   }),
+
+   check('userType')
+   .notEmpty()
+   .withMessage("please select user type")
+   .isIn(['guest','host'])
+   .withMessage("invalid user type"),
+
+   check("policies")
+   .notEmpty()
+   .withMessage("please accept the terms an conditions")
+   .custom((value,{req})=>{
+      if( value !== "on"){
+         throw new Error("pleaase accect the terms and conditions")
+      }
+      return true;
+   }),
+   (req, res, next) => {
+      
+   const {firstName, lastName, email, password, userType} = req.body
+   const errors = validationResult(req)
+   if(!errors.isEmpty()){
+      return res.status(422).render('auth/signup',{
+         isLoggedIn: false,
+         errors: errors.array().map(err=>err.msg),
+         oldInput: {firstName, lastName, email, password, userType}
+      })
+   }
+
+  res.redirect('/login')
+
+}];
 
 exports.loggedin=(req,res,next) =>{
   console.log(req.body)
@@ -24,15 +97,14 @@ exports.loggedout=(req,res,next)=>{
 }
 
 exports.getsignup = (req, res, next) => {
-   res.render("auth/signup",{isLoggedIn : req.isLoggedIn});
+   res.render("auth/signup",{
+      isLoggedIn : req.isLoggedIn,
+      errors :[],
+      oldInput : {firstName:"", lastName:"", email:"", password:"", userType:""}
+   });
 
 
 };
 
 
 
-exports.postsignup = (req, res, next) => {
-   res.render("auth/signup",{isLoggedIn : req.isLoggedIn});
-
-
-};
